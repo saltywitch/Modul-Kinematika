11677062f646074ab7c8db89d999fc7fa2cfbd33
saltywitch <98076828+saltywitch@users.noreply.github.com>
2024-07-31 04:32:54 +0800

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,174 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6nXrEbWzAta":
+        Script1();
+        break;
+      case "5sMflTB29sF":
+        Script2();
+        break;
+      case "6boYeDlYcDf":
+        Script3();
+        break;
+      case "5XcVyBGerqA":
+        Script4();
+        break;
+      case "6UJHKd4PJ1E":
+        Script5();
+        break;
+      case "5jYUvgbtFYT":
+        Script6();
+        break;
+      case "5yoPp7e061f":
+        Script7();
+        break;
+      case "6NHxfDH55zX":
+        Script8();
+        break;
+      case "6WhQ3earcYN":
+        Script9();
+        break;
+      case "6SMDYupqxNY":
+        Script10();
+        break;
+      case "5kJjRNJJK4B":
+        Script11();
+        break;
+      case "5tpnrwqeQkd":
+        Script12();
+        break;
+      case "6Vm6JsufyY9":
+        Script13();
+        break;
+      case "5VqY6evoase":
+        Script14();
+        break;
+      case "6cEjRr4wQns":
+        Script15();
+        break;
+      case "6P8fcwLAtTI":
+        Script16();
+        break;
+      case "6etbTWtKazY":
+        Script17();
+        break;
+      case "5oRxtdJoZxP":
+        Script18();
+        break;
+      case "6IkWyUXZESi":
+        Script19();
+        break;
+      case "6gLSJoOGuT7":
+        Script20();
+        break;
+      case "5nXSSCX9uUg":
+        Script21();
+        break;
+      case "5lIpICii3tL":
+        Script22();
+        break;
+      case "5auZM0kQUox":
+        Script23();
+        break;
+      case "6Yq6Drd0E26":
+        Script24();
+        break;
+      case "5yHFZVcY8tz":
+        Script25();
+        break;
+      case "6ap4XxDOTca":
+        Script26();
+        break;
+      case "67VQzfr9gL4":
+        Script27();
+        break;
+      case "6IjNnNYHn4v":
+        Script28();
+        break;
+      case "5y7FyAMQQri":
+        Script29();
+        break;
+      case "5WUXiSLYliV":
+        Script30();
+        break;
+      case "6fUX23c5HDP":
+        Script31();
+        break;
+      case "6Li9wywO0C4":
+        Script32();
+        break;
+      case "5rT8xMS0VJr":
+        Script33();
+        break;
+      case "60EagUdwQEU":
+        Script34();
+        break;
+      case "6Bn2U5RQM1s":
+        Script35();
+        break;
+      case "5WiQk2EtZKT":
+        Script36();
+        break;
+      case "6oHWm2jVqK1":
+        Script37();
+        break;
+      case "62a27QdGxSC":
+        Script38();
+        break;
+      case "6OTgu6XAEKL":
+        Script39();
+        break;
+      case "616fgg6P6Gg":
+        Script40();
+        break;
+      case "5rzwM2amjf6":
+        Script41();
+        break;
+      case "6Rg90o9U4Jy":
+        Script42();
+        break;
+      case "6kSqMFTxjjP":
+        Script43();
+        break;
+      case "68ypc8ny1hS":
+        Script44();
+        break;
+      case "5lQTMA84w0i":
+        Script45();
+        break;
+      case "6fNC9SfeLee":
+        Script46();
+        break;
+      case "6MJwHR8bHsW":
+        Script47();
+        break;
+      case "6R5TQZEN80h":
+        Script48();
+        break;
+      case "6BojJsu2LAD":
+        Script49();
+        break;
+      case "5ZshBUES3FX":
+        Script50();
+        break;
+      case "6mkVL93Qnzm":
+        Script51();
+        break;
+      case "6hg9lgzLzm5":
+        Script52();
+        break;
+      case "5fgDI21aux5":
+        Script53();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
