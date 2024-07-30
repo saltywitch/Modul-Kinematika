c57d83e8a3ef50b8be59028aa042fa2874603be8
saltywitch <98076828+saltywitch@users.noreply.github.com>
2024-07-30 09:00:17 +0800

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,21 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6eHX0HLe3ze":
+        Script1();
+        break;
+      case "6lsBjfJ6C5X":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
